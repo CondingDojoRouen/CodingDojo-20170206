61ef8993c7167f9c1c6b86168750270ba73e9b5b
Clément Mauger <cmauger@osiarouen.onmicrosoft.com>
2017-02-06 17:58:17 +0100

    Init project

---
+++ tests/BankOCRTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankOCRTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
