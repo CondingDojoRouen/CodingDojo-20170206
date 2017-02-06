c23d5de57807bda97de15921dd27c27c2e3ec240
Antoine <antoine.benard@econocom.com>
2017-02-06 18:39:06 +0100

    Pass test ThrowsFileNotFoundExceptionIfFileDoesntExists

---
+++ src/BankOCR/BankOCR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOCR
+{
+    public static class BankOCR
+    {
+        public static void DecodeFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException();
+            
+        }
+    }
+}
---
+++ tests/BankOCRTests/BankOCRTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace BankOCR
+{
+    [TestClass]
+    public class BankOCRTests
+    {
+        [TestMethod]
+        public void ThrowsFileNotFoundExceptionIfFileDoesntExists()
+        {
+            // Arrange
+            string filePath = string.Empty;
+            
+            // Act
+            
+            //Assert
+            Assert.ThrowsException<FileNotFoundException>(() => BankOCR.DecodeFile(filePath));
+        }
+    }
+}
